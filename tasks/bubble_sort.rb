0cfe4b3f47d4f696218e03989d9fef02d3d5ed2e
whiskey <renerriad@gmail.com>
2021-03-24 10:47:28 -0400

    uhh, commit?

---
+++ tasks/bubble_sort.rb
@@ -0,0 +1,32 @@
+require 'byebug'
+
+arr = []
+
+99.times do 
+	arr << rand(1..100)
+end
+
+
+ii = 0
+j = 1
+tmp = 0
+
+puts arr
+
+puts '----'
+len = arr.length
+
+while ii < len
+	j = 1
+	while j < (len - ii)
+		if arr[j-1] < arr[j]
+			tmp = arr[j]
+			arr[j] = arr[j-1]
+			arr[j-1] = tmp
+		end
+	j = j + 1
+	end
+	ii = ii + 1
+end
+
+puts arr
---
+++ tasks/selective_sort.rb
@@ -0,0 +1,35 @@
+require 'byebug'
+
+arr = []
+
+10.times do 
+	arr << rand(1..100)
+end
+
+
+ii = 0
+j = 1
+tmp = 0
+
+puts arr
+
+puts '----'
+len = arr.length
+
+while ii < len - 1
+	j = ii + 1
+	plus_petit = ii
+	while j < (len)
+		if arr[plus_petit] > arr[j]
+			plus_petit = j
+		end
+		j = j + 1
+	end		
+	tmp = arr[ii]
+	arr[ii] = arr[plus_petit]
+	arr[plus_petit] = tmp
+
+	ii = ii + 1
+end
+
+puts arr
